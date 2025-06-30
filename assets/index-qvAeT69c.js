93e012a92e27ba504f2b5132ddf966c5979887a4
David Quispe <jloo6778@gmail.com>
2025-06-30 15:15:26 -0500

    Add html5-qrcode dependency, implement QR code scanning for document number input, and refactor related components for improved user experience

---
+++ assets/index-qvAeT69c.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-C31eAhYF.js";const o=e({__name:"index",setup(n){return(t,r)=>null}});export{o as default};
---
+++ assets/usePageMeta-CN2GGemH.js
@@ -0,0 +1,1 @@
+import{by as n}from"./index-C31eAhYF.js";function i(t,e,o,r){n({title:t,meta:[{name:"description",content:e},{property:"og:title",content:t},{property:"og:description",content:e},{property:"og:image",content:"https://tusitio.com/logo.png"}],link:o?[{rel:"icon",href:`/${o}`}]:[]})}export{i as u};
