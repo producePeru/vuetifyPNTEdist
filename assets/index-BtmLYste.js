9b0658efb2512e7e0693e42854795bcd0b7331ac
David Quispe <jloo6778@gmail.com>
2025-06-30 16:11:40 -0500

    Refactor QR scanner logic: streamline loading state management and clean up commented code for improved readability.

--- assets/index-BtmLYste.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-CA0A6pts.js";const o=e({__name:"index",setup(n){return(t,r)=>null}});export{o as default};
---
+++ assets/index-CvBBx8rI.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-DG14tgpm.js";const o=e({__name:"index",setup(n){return(t,r)=>null}});export{o as default};
