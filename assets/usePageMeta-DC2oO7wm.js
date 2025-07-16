54ac1687eec698c31bee37e6893de90cffe01b7a
David Quispe <jloo6778@gmail.com>
2025-07-16 17:22:36 -0500

    feat: Update Cyber Pnte page layout and card display for improved user experience

---
+++ assets/usePageMeta-DC2oO7wm.js
@@ -0,0 +1,1 @@
+import{aH as n}from"./index-szycFpCk.js";function a(t,e,o,r){n({title:t,meta:[{name:"description",content:e},{property:"og:title",content:t},{property:"og:description",content:e},{property:"og:image",content:"https://tusitio.com/logo.png"}],link:o?[{rel:"icon",href:`/${o}`}]:[]})}export{a as u};
