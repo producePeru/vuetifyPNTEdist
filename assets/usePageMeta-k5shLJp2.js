45421184832fe6f017989511f3576f5d0b7572c8
Elizabeth Holmes <eliza@pnte.com>
2025-08-13 17:09:34 -0500

    fix: Update GranosAnimacion properties for improved visual effects and adjust index page layout for better presentation

--- assets/usePageMeta-k5shLJp2.js
+++
@@ -1,1 +0,0 @@
-import{bL as n}from"./index--lHd8asV.js";function i(t,e,o,r){n({title:t,meta:[{name:"description",content:e},{property:"og:title",content:t},{property:"og:description",content:e},{property:"og:image",content:"https://tusitio.com/logo.png"}],link:o?[{rel:"icon",href:`/${o}`}]:[]})}export{i as u};
