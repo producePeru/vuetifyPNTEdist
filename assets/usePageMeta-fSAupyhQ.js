a2c6cff862fc6c7bf3e6938489695f60c1bce145
Developer <developer_factory@tuempresa.com>
2026-04-08 15:36:54 -0500

    app

---
+++ assets/usePageMeta-fSAupyhQ.js
@@ -0,0 +1,1 @@
+import{bZ as n}from"./index-c9MCWKnM.js";function i(t,e,o,r){n({title:t,meta:[{name:"description",content:e},{property:"og:title",content:t},{property:"og:description",content:e},{property:"og:image",content:"https://tusitio.com/logo.png"}],link:o?[{rel:"icon",href:`/${o}`}]:[]})}export{i as u};
